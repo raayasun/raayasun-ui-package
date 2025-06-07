9a9153d9a92457a64e6b9fe129e0eae59dc02df3
Mohammad <slymanyanmhmd34@gmail.com>
2025-06-07 15:49:27 +0330

    Added Component SomeBtnMini.jsx

---
+++ src/components/Buttons/SomeBtnMini/SomeBtnMini.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './SomeBtnMini.module.css'
+import {Link} from "react-router-dom";
+
+const SomeBtnMini = ({title, imageUrl, href, iconUrl}) => {
+    return (
+        <Link to={href}
+              className={`d-flex align-items-center justify-content-between  text-decoration-none fw-bold ${styles.card}`}>
+            <div className={"d-flex align-items-center "}>
+                {imageUrl && <img src={imageUrl} alt={title} className={`mx-auto ${styles.image}`}/>}
+                <h3 className={` text-center fw-bold m-0 ms-2 ${styles.title}`}>{title}</h3>
+            </div>
+            {iconUrl && <img src={iconUrl} alt={title} className={`${styles.icon}`}/>}
+        </Link>
+    );
+};
+export default SomeBtnMini;
