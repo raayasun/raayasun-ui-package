d07d8128bcf80041d3efcfd9cf3d21ab8b8c7725
Mohammad <slymanyanmhmd34@gmail.com>
2025-06-07 15:50:37 +0330

    Added Component and style Input.jsx

---
+++ src/components/Inputs/FloatingInputs/Input.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Input.module.css'
+import {Link} from "react-router-dom";
+import {FloatingLabel, Form} from "react-bootstrap";
+
+const Input = ({type, placeholder, label, value , name}) => {
+    return (
+        <FloatingLabel
+            label={label}
+            className="mb-3"
+        >
+            <Form.Control type={type} placeholder={placeholder} value={value} name={name} className={styles.input}/>
+        </FloatingLabel>
+    );
+};
+export default Input;
