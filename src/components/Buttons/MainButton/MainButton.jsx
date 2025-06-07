a7948db2d371c72342a21ca1465c169d800a6d21
Mohammad <slymanyanmhmd34@gmail.com>
2025-06-07 15:50:25 +0330

    Added Component and style MainButton.jsx

--- src/components/Buttons/MainButton/MainButton.jsx
+++ src/components/Buttons/MainButton/MainButton.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import styles from './CardButton.module.css';
+import styles from './MainButton.module.css';
 import {Link} from "react-router-dom";
 
-const CardButton = ({title, imageUrl, navigateUrl}) => {
+const MainButton = ({title, imageUrl, href}) => {
     return (
-        <Link to={navigateUrl} className={`d-flex flex-column justify-content-around rounded-4 text-decoration-none fw-bold ${styles.card}`}>
+        <Link to={href}
+              className={`d-flex flex-column justify-content-around  text-decoration-none fw-bold ${styles.card}`}>
             {imageUrl && <img src={imageUrl} alt={title} className={`mx-auto ${styles.image}`}/>}
             <h3 className={`mt-3 text-center fw-bold ${styles.title}`}>{title}</h3>
         </Link>
     );
 };
 
-export default CardButton;
+export default MainButton;
