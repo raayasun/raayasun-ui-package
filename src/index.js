a048ee7d870c59863a0e26027ce73e54658e4de7
Mohammad <slymanyanmhmd34@gmail.com>
2025-06-07 16:23:24 +0330

    add

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-
-export { default as MainButton } from './components/Buttons/MainButton/MainButton.jsx';
-export { default as EventBtn } from './components/Buttons/EventBtn/EventBtn.jsx';
+export {default as MainButton} from './components/Buttons/MainButton/MainButton.jsx';
+export {default as EventBtn} from './components/Buttons/EventBtn/EventBtn.jsx';
+export {default as SomeBtn} from './components/Buttons/SomeBtn/SomeBtn.jsx';
+export {default as SomeBtnMini} from './components/Buttons/SomeBtnMini/SomeBtnMini.jsx';
+export {default as MainCard} from './components/Cards/MainCard.jsx';
+export {default as Input} from './components/Inputs/FloatingInputs/Input.jsx';
 
