885fc6bfe2c377c61915b4c359ced9d9d64da5b5
Mohammad <slymanyanmhmd34@gmail.com>
2025-08-22 12:14:17 +0330

    Fixed Bugs

--- src/components/Buttons/MainBtn/MainButton.jsx
+++ src/components/Buttons/MainBtn/MainButton.jsx
@@ -1,17 +1,17 @@
-import React, { useContext } from 'react';
+import React, {useContext} from 'react';
 import styles from './MainButton.module.css';
-import {UNSAFE_RouteContext } from "react-router-dom";
+import {UNSAFE_RouteContext} from "react-router-dom";
 
-const MainButton = ({ title, imageUrl }) => {
+const MainButton = ({title, imageUrl}) => {
     const routerContext = useContext(UNSAFE_RouteContext); // محافظت در برابر نبود Router
 
     if (!routerContext) return null; // از رندر خارج شو اگر Router وجود نداشت
 
     return (
         <div
-              className={`d-flex flex-column justify-content-around text-decoration-none fw-bold ${styles.card}`}>
-            {imageUrl && <img src={imageUrl} alt={title} className={`mx-auto ${styles.image}`} />}
-            <h3 className={`mt-3 text-center fw-bold ${styles.title}`}>{title}</h3>
+            className={`d-flex flex-column justify-content-around text-decoration-none fw-bold ${styles.card}`}>
+            {imageUrl && <img src={imageUrl} alt={title} className={`mx-auto ${styles.image}`}/>}
+            <h3 className={`mt-3 text-center ${styles.title}`}>{title}</h3>
         </div>
     );
 };
